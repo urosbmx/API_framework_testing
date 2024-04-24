da63b727510a434267e817c607a87ebfa0b7effc
urosb <urosbmx@gmail.com>
2024-04-24 20:32:21 +0200

    Update log

--- Tests/test_links/test_links.py
+++ Tests/test_links/test_links.py
@@ -1,11 +1,16 @@
 import pytest
 import requests
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Test_links_all_sites:
 
     @pytest.mark.xray("API-1")
-    def test_all_links(self):
+    def test_all_links(self,caplog):
         api_url = f"https://jsonplaceholder.typicode.com/posts"
+        logger.info("Test Started")
         r = requests.get(api_url)
+        logger.info(f"Validation status code {r.status_code}")
         assert r.status_code == 200
--- conftest.py
+++ conftest.py
@@ -1,4 +1,25 @@
 import os
 from dotenv import load_dotenv, dotenv_values
+import logging
+import pytest
+from pytest_xray import evidence
+
+
+@pytest.fixture(scope='session')
+def setup_logging():
+    logging.basicConfig(level=logging.INFO)
+
+
 load_dotenv()
 print(os.getenv("XRAY_API_BASE_URL"))
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    evidences = getattr(report, "evidences", [])
+    if report.when == "call":
+        data = open("myoutput.log", "rb").read()
+        evidences.append(evidence.jpeg(data=data, filename="myoutput.log"))
+    report.evidences = evidences
