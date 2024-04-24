0d55d491489719e8df1243717250208234468640
urosb <urosbmx@gmail.com>
2024-04-24 21:26:14 +0200

    Update log

--- Tests/test_todo_domen/test_todo_domen.py
+++ Tests/test_todo_domen/test_todo_domen.py
@@ -10,16 +10,22 @@ import os
 # LINK https://jsonplaceholder.typicode.com
 
 
+logger = logging.getLogger(__name__)
+
+
 class Test_JsonPlaceHolder:
 
     @pytest.mark.xray("API-7")
     def test_GET(self):
         api_url = "https://jsonplaceholder.typicode.com/todos/1"
-        logging.info("Test Started")
+        logger.info("Test Started")
         r = requests.get(api_url)
         data = r.json()
+        logger.info(f"Test status code {r.status_code}")
         assert r.status_code == 200
+        logger.info(f"Test Content-Type {r.headers["Content-Type"]}")
         assert r.headers["Content-Type"] == "application/json; charset=utf-8"
+        logger.info(f"Test response title --> {data["title"]}")
         assert data["title"] == "delectus aut autem"
 
     @pytest.mark.xray("API-8")
@@ -27,7 +33,9 @@ class Test_JsonPlaceHolder:
         api_url = f"https://jsonplaceholder.typicode.com/todo"
         logging.info("Test Started")
         r = requests.get(api_url)
+        logger.info(f"Test Content-Type {r.headers["Content-Type"]}")
         assert r.headers["Content-Type"] == "application/json; charset=utf-8"
+        logger.info(f"Test status code {r.status_code}")
         assert r.status_code == 404
 
     @pytest.mark.xray("API-9")
@@ -36,8 +44,9 @@ class Test_JsonPlaceHolder:
         r = requests.get(api_url)
         logging.info("Test Started")
         data = r.json()
+        logger.info(f"Test Content-Type {r.headers["Content-Type"]}")
         assert r.headers["Content-Type"] == "application/json; charset=utf-8"
-        logging.info("Test checking status code")
+        logging.info(f"Test checking status code {r.status_code}")
         assert r.status_code == 200
         logging.info("Test checking count of element in array")
         assert len(data) == 10
--- conftest.py
+++ conftest.py
@@ -5,11 +5,19 @@ import pytest
 from pytest_xray import evidence
 
 
-@pytest.fixture(scope='session')
-def setup_logging():
-    logging.basicConfig(level=logging.INFO)
+
 
 
 load_dotenv()
 print(os.getenv("XRAY_API_BASE_URL"))
 
+# @pytest.hookimpl(hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     report = outcome.get_result()
+#     evidences = getattr(report, "evidences", [])
+#     if report.when == "call":
+#
+#         data = open("myoutput.txt", "rb").read()
+#         evidences.append(evidence.text(data=data, filename="myoutput.txt"))
+#     report.evidences = evidences
